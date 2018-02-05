d5c89d13b379c6a17dced83be84e6a4f57f63ca2
etkonrad19 <34580515+etkonrad19@users.noreply.github.com>
2018-02-05 12:03:17 -0500

    Update adventure game.py

--- adventure game.py
+++ adventure game.py
@@ -1,14 +1,14 @@
-print("you're in a dark hallway")
-choice = bool(input(" True or False: Do you want to walk down the dark hallway?"))
-if choice == True:
-    print ("walk down the hallway")
-    print ("The secret door opens up and you fall in")
+print("You're in a dark hallway")
+choice = str(input(" True or False: Do you want to walk down the dark hallway?"))
+if choice == "Yes":
+    print ("Walk down the hallway.")
+    print ("The secret door opens up and you fall in.")
     
 else:
-    print ("You dont have any other option my Dude still send it")
-    print (" caution we have detected unknown creature")
-    print ("Theres no going back!!")
-    print ("The secret door opens up and you fall in")
+    print ("You dont have any other option my Dude still send it.")
+    print ("Caution we have detected unknown creature.")
+    print ("There is no going back!!")
+    print ("The secret door opens up and you fall in.")
     
 
     
