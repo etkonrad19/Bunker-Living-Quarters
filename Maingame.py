9d14eb902538857ad91fe55b88c20578c9b669b4
etkonrad19 <34580515+etkonrad19@users.noreply.github.com>
2018-02-05 12:06:31 -0500

    Create Maingame.py

---
+++ Maingame.py
@@ -0,0 +1,12 @@
+def hallway():
+    print("You're in a dark hallway")
+    choice = str(input(" True or False: Do you want to walk down the dark hallway?"))
+    if choice == "Yes":
+        print ("Walk down the hallway.")
+        print ("The secret door opens up and you fall in.")
+
+    else:
+        print ("You dont have any other option my Dude still send it.")
+        print ("Caution we have detected unknown creature.")
+        print ("There is no going back!!")
+        print ("The secret door opens up and you fall in.")
