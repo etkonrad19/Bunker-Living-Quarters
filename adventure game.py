6dcfcdf2b53821ed111fa7d1cede6f145d003796
patrick Habimana <34580568+pahabimana19@users.noreply.github.com>
2018-02-05 11:43:05 -0500

    very nice hallway

---
+++ adventure game.py
@@ -0,0 +1,15 @@
+print("you're in a dark hallway")
+choice = bool(input(" True or False: Do you want to walk down the dark hallway?"))
+if choice == True:
+    print ("walk down the hallway")
+    print ("The secret door opens up and you fall in")
+    
+else:
+    print ("You dont have any other option my Dude still send it")
+    print (" caution we have detected unknown creature")
+    print ("Theres no going back!!")
+    print ("The secret door opens up and you fall in")
+    
+
+    
+    
---
+++ next hallway.py
@@ -0,0 +1,52 @@
+import random 
+playerLocation = "square1"
+escaped= False 
+while escaped == False:
+    playerInput = str(input("what do you want to do?"))
+If playerLocatin == "square1":
+    searchNorth = "Nothing to search my dude"
+    searchEast = "Nothing to search my dude"
+    searchSouth = "Nothing to search my dude"
+    searchWest = "Nothing to search my dude"
+    
+    moveNorth = "sqaure2"
+    moveEast = "wall"
+    moveSouth = "wall"
+    moveWest= "wall"
+    
+    lookNorth = "Where you're going"
+    lookEast = "there;'s a wall"
+    lookSouth = "The door you came in through"
+    lookWest = "there;'s a wall"
+    
+    interactNorth = "Nothing to interact with "
+    interactEast = "Nothing to interact with"
+    interactSouth = "Nothing to interact with"
+    interactWest = "Nothing to interact with"
+    
+    textNorth = "You walking down the halway "
+    textEast = ""
+    textSouth = ""
+    textWest = ""
+
+searches = lookNorth, lookEast,lookSouth,lookWest
+if player searches 
+   print ("suprise you found a rock")
+   print ("Hold on to that, thats your key to get outoff here") 
+  playerInputForTheRock =  print str(input("would you like to throw the rock at the lever"))
+    if playerInputForTheRock = "yes"
+        throw = random.randrange(1,3)
+        if throw == 1:
+            print(" the lever opened you are free to go")
+            print(" Now LEAVEEEEEE")
+            else:
+                print("you failed to through the rock hard enough please try again") 
+    
+        
+        
+        
+        
+        
+        
+
+   
